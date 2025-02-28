9440977f1384ca6246509483662251c7873d6960
Kethlen Martins <kethlen.martins21@gmail.com>
2025-02-28 12:20:47 -0300

    feat: bugfix

--- scenes/game.js
+++ scenes/game.js
@@ -46,31 +46,6 @@ export class GameScene extends Phaser.Scene {
             this.scene.start("EndScene", {resultado: resultado});
         })
         // quarta parte
-      
-        // Animações da personagem
-        this.anims.create ({
-            key: 'direita',
-            frame: this.anims.generateFrameNumbers('grace_sprite', {start: 0, end: 3}),
-            frameRate: 10,
-            repeat: -1
-        })
-
-        this.player.anims.play('direita');
-
-        this.anims.create ({
-            key: 'esquerda',
-            frame: this.anims.generateFrameNumbers('grace_sprite', {start: 5, end: 8}),
-            frameRate: 10,
-            repeat: -1
-        })
-        this.player.anims.play('esquerda');
-
-        this.anims.create({
-            key: 'parada',
-            frames: [ { key: 'grace_sprite', frame: 4 } ],
-            frameRate: 20
-        });
-        this.player.anims.play('parada');
 
         // adicionando placar 
         this.placar = this.add.text(50, 50, 'Pontuacao:' + this.pontuacao, {fontSize:'45px', fill:'#495613'});
@@ -99,23 +74,45 @@ export class GameScene extends Phaser.Scene {
 
         });
 
+    
+        // Animações da personagem
+        this.anims.create({
+            key: 'direita',
+            frames: this.anims.generateFrameNumbers('grace_sprite', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'esquerda',
+            frames: this.anims.generateFrameNumbers('grace_sprite', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'parada',
+            frames: [{ key: 'grace_sprite', frame: 4 }],
+            frameRate: 20
+        });
     }
 
-    update() { // Adiciona a movimentação 
-        if (this.cursors.left.isDown) { // Movimentação horizontal
+    update() {
+        if (this.cursors.left.isDown) {
             this.player.setVelocityX(-160);
             if (this.player.anims.currentAnim?.key !== 'esquerda') {
-                this.player.anims.play ('esquerda', true); }
-
+                this.player.anims.play('esquerda', true);
+            }
         } else if (this.cursors.right.isDown) {
             this.player.setVelocityX(160);
             if (this.player.anims.currentAnim?.key !== 'direita') {
-                this.player.anims.play ('direita', true); }
-
+                this.player.anims.play('direita', true);
+            }
         } else {
             this.player.setVelocityX(0);
             if (this.player.anims.currentAnim?.key !== 'parada') {
-                this.player.anims.play ('parada', true); }
+                this.player.anims.play('parada', true);
+            }
         }
 
         // Lógica de pulo (vertical) 
